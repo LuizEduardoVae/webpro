5b089c883fcb6d4af09cd9006f0ee7c5fbbc1ed4
Luiz Eduardo Vedoato Almeida Everton <luizeduardovedoatoalmeidaeverton@192.168.0.18>
2025-04-22 22:39:17 -0300

    Paper6

--- app/components/pages/project/project-sections/index.tsx
+++ app/components/pages/project/project-sections/index.tsx
@@ -2,28 +2,33 @@ import Image from "next/image"
 import { ProjectSection } from "@/app/types/projects"
 
 type ProjectSectionsProps = {
-    sections: ProjectSection[]
+    sections?: ProjectSection[]
 }
 
-export const ProjectSections = ({sections}: ProjectSectionsProps) => {
-    return(
+export const ProjectSections = ({ sections }: ProjectSectionsProps) => {
+    if (!Array.isArray(sections)) return null;
+
+    return (
         <section className="container my-12 md:my-32 flex flex-col gap-8 md:gap-32">
-            {sections.map(section => (
-                <div key={section.title} className=" flex flex-col items-center gap-6 md:gap-12">
-                    <h2 className="text-2xl md:text-3xl text-white">
-                        {section.title}
-                    </h2>
-                    <Image 
-                        src={section.image.url}
-                        width={1080}
-                        height={672}
-                        alt={`Imagem da Sessão $(section.title)`}
-                        className="w-full aspect-auto rounded-lg object-cover"
-                        unoptimized>
-                    </Image>
+            {sections.map((section, index) => (
+                <div key={section.title || index} className="flex flex-col items-center gap-6 md:gap-12">
+                    {section.title && (
+                        <h2 className="text-2xl md:text-3xl text-white">
+                            {section.title}
+                        </h2>
+                    )}
+                    {section.image?.url && (
+                        <Image
+                            src={section.image.url}
+                            width={1080}
+                            height={672}
+                            alt={`Imagem da Sessão ${section.title || ''}`}
+                            className="w-full aspect-auto rounded-lg object-cover"
+                            unoptimized
+                        />
+                    )}
                 </div>
             ))}
-
         </section>
     )
 }
