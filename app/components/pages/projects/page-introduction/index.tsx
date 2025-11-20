1c8ce028b00098881d40d5973ba02df6a7e36e08
Luiz Eduardo Vedoato <luizeduardovedoato@192.168.0.18>
2025-11-19 22:46:33 -0300

    n3

--- app/components/pages/projects/page-introduction/index.tsx
+++ app/components/pages/projects/page-introduction/index.tsx
@@ -28,6 +28,8 @@
 
 
 
+'use client';
+
 import { Link } from "@/app/components/link";
 import { SectionTitle } from "@/app/components/section-title";
 import { HiArrowNarrowLeft } from "react-icons/hi";
