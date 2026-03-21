5f69c35b051cfbf60626de52817ee81e47f13dc5
Luiz Eduardo Vedoato <luizeduardovedoato@MacBook-Pro-de-Luiz.local>
2026-03-21 10:55:10 -0300

    seoa

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
-import { MetadataRoute } from 'next';
-import { fetchHygraphQuery } from './utils/fetch-hygraph-query';
-import { ProjectsPageStaticData } from './types/page-info';
+import { MetadataRoute } from "next";
+import { fetchHygraphQuery } from "./utils/fetch-hygraph-query";
+import { ProjectsPageStaticData } from "./types/page-info";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   let projectEntries: MetadataRoute.Sitemap = [];
@@ -18,9 +18,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     if (data && data.projects) {
       projectEntries = data.projects.map((project) => ({
-        url: \`https://luizvedoatowebsite.vercel.app/projects/\${project.slug}\`,
+        url: `https://luizvedoatowebsite.vercel.app/projects/${project.slug}`,
         lastModified: new Date(),
-        changeFrequency: 'weekly',
+        changeFrequency: "weekly",
         priority: 0.8,
       }));
     }
@@ -30,15 +30,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: 'https://luizvedoatowebsite.vercel.app',
+      url: "https://luizvedoatowebsite.vercel.app",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: 'https://luizvedoatowebsite.vercel.app/projects',
+      url: "https://luizvedoatowebsite.vercel.app/projects",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.9,
     },
     ...projectEntries,
